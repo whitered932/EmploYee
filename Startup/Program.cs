8fbe7d28d48408145b988dc0b47566ed8b6df7ea
whitered932 <dev@whitered932.ru>
2023-12-11 21:06:33 +0300

    asdf

--- Startup/Program.cs
+++ Startup/Program.cs
@@ -12,16 +12,17 @@ builder.Services.AddMediatR(typeof(Program).Assembly);
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
-        policy  =>
-        {
-            policy.WithOrigins("http://example.com").AllowAnyHeader().AllowAnyMethod();
-        });
+        policy => { policy.WithOrigins("http://example.com").AllowAnyHeader().AllowAnyMethod(); });
 });
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 var app = builder.Build();
 
-app.UseCors();
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
